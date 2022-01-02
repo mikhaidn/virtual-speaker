cee39c71038ad3ee3cc39d74dda5cf27aec460fe
plokmin <danmikhail@gmail.com>
2022-01-01 21:08:38 -0600

    web

--- sessionauth.js
+++
@@ -1,7 +0,0 @@
-window.onSpotifyWebPlaybackSDKReady = () => {
-  const token = 'BQCrZzxaJdEWz7qbRR8XkY2ZpMKa_8V3L5wewht9RaeJLwHkFRr6UKfcLXdQszlMXX3DbehZomUhz27lMW1-mp9CIFeATQ_7J4Vk7bZ8ip_ZRVapwItmsyCddRwW6orHVQK1drZDfGFan12EwLCDBT_8qGVZ1PEhLis';
-  const player = new Spotify.Player({
-    name: 'Web Playback SDK Quick Start Player',
-    getOAuthToken: cb => { cb(token); },
-    volume: 0.5
-  });
